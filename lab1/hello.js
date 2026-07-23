82da129cce7df29982d3906958e2c945bef7975f
Aditya kasaudhan <ag4114365@gmail.com>
2026-07-23 11:53:59 +0530

    initial commit

---
+++ lab1/hello.js
@@ -0,0 +1,13 @@
+console.log("Aditya Kasaudhan");
+//write a function to take two numer ad return their sum
+function sum(a, b){
+    return a+b; 
+}
+const add = (a, b) => {
+    return a+b; 
+}
+const add2 = (a, b) => a+b;
+
+console.log(sum(20, 10));
+console.log(add(20, 10));
+console.log(add2(20, 10));
